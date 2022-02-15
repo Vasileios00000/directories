90e9c24e5807c0c644618803d1440fe0f4fb0538
VasilisG <vasilisgeorgilas@gmail.com>
2022-02-15 13:12:52 +0200

    root_finished

--- src/components/Item.js
+++ src/components/Item.js
@@ -10,6 +10,41 @@ class Item extends Component {
 
     item =this.props.item
 
+    renderItemIcon(type)
+    {
+        if(type=='directory'){
+
+            return(<span class="material-icons">folder_open</span>)
+        }
+
+        if(type=='image'){
+
+            return(<span class="material-icons">image</span>)
+        }
+        if(type=='file'){
+
+            return(<span class="material-icons">insert_drive_file</span>)
+        }
+        else
+        {
+
+            return(<span >Icon not found</span>)
+        }
+         
+    }
+
+    renderName(name){
+        if (name.length>13){
+            const name_cut=name.slice(0, 8)
+            return (<span>{name_cut}...</span>)
+        }
+        else{
+            return(<span>{name}</span>)
+        }
+    }
+
+    
+
 
 
     render() {
@@ -24,16 +59,18 @@ class Item extends Component {
 
         if(photo_ext=='.jpg')
         {
-            type='photo'
+            type='image'
         }
 
 
         return (
             <div className={classes.item}>
-                {type}
-                
-                {this.item.name}
-                
+                <div>
+                    {this.renderItemIcon(type)}
+                </div>
+                <div>
+                     {this.renderName(this.item.name)}
+                </div>
             </div>
         );
     }
